e3700fd00315be22c632a8bc4790eeb29a29e1d2
codejunkie99 <email@email.com>
2026-04-28 18:04:26 +0530

    Update formula for v0.2.1

--- Formula/ztk.rb
+++ Formula/ztk.rb
@@ -1,8 +1,8 @@
 class Ztk < Formula
   desc "CLI proxy that reduces LLM token consumption by 78%+. Zero dependencies."
   homepage "https://github.com/codejunkie99/ztk"
-  url "https://github.com/codejunkie99/ztk/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2d42c4ca868be389442397b3bc7edc84d71c0c0a7b3188d50185a7daeb8ed813"
+  url "https://github.com/codejunkie99/ztk/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "5fd42868cd4cf56842893326efd9f9c3be5e138bde06e56d52abd78c5b67c577"
   license "MIT"
 
   depends_on "zig" => :build
@@ -15,6 +15,6 @@ class Ztk < Formula
   end
 
   test do
-    assert_match "ztk 0.2.0", shell_output("#{bin}/ztk --version")
+    assert_match "ztk 0.2.1", shell_output("#{bin}/ztk --version")
   end
 end
